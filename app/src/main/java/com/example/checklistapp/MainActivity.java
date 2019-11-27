45c1b46d6b5e9169513356ed7597517569e1ccda
Leandro-SI <llmanutencao@bol.com.br>
2019-11-27 19:12:18 -0300

    teste

--- app/src/main/java/com/example/checklistapp/MainActivity.java
+++ app/src/main/java/com/example/checklistapp/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.checklistapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
