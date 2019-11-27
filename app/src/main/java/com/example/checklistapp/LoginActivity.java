2f821efc9ea881d369d6bab24c91204c37bcaf74
Leandro-SI <llmanutencao@bol.com.br>
2019-11-27 19:58:13 -0300

    telas boas vindas e login

--- app/src/main/java/com/example/checklistapp/LoginActivity.java
+++ app/src/main/java/com/example/checklistapp/LoginActivity.java
@@ -3,19 +3,12 @@ package com.example.checklistapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
-import java.util.Random;
-
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
+        setContentView(R.layout.activity_login);
     }
 }
